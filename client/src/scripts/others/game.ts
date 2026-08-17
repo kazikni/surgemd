1f77aabc747f9efafb934e11d93749e9985bafd7
Kazikni <canaldokaklik@gmail.com>
2026-08-17 20:41:10 -0300

    fix:client_side_rotation

--- client/src/scripts/others/game.ts
+++ client/src/scripts/others/game.ts
@@ -421,7 +421,7 @@ export class Game extends ClientGame<GameObject>{
         if(!this.active_entity)return
         this.input.angle=angle
         this.input.distance_to_aim=dist
-        if(!this.active_entity.downed&&!this.active_entity.swimming&&this.active_entity.controlling&&!this.active_entity.seat&&this.save.get_variable("sv_game_client_rot")&&!this.game_over){
+        if(!this.active_entity.downed&&!this.active_entity.swimming&&!this.active_entity.seat&&this.save.get_variable("sv_game_client_rot")&&!this.game_over){
             this.active_entity.enable_auto_rot=false
             this.active_entity.physical_data.rotation=this.input.angle
         }else{
